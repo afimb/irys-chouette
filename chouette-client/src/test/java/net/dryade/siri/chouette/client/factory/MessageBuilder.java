87d9568eaa8ca3997344f59ada948160c5174c01
Marc Florisson <marc.florisson@dryade.net>
2011-12-14 11:41:42 +0100

    write string in UTF8

--- chouette-client/src/test/java/net/dryade/siri/chouette/client/factory/MessageBuilder.java
+++ chouette-client/src/test/java/net/dryade/siri/chouette/client/factory/MessageBuilder.java
@@ -19,7 +19,7 @@ public class MessageBuilder {
 
     public static MessageBuilder create() {
         return new MessageBuilder( MessageType.shortMessage,
-                                   "Trafic devi",
+                                   "Trafic devié",
                                    "FR");
     }
     
